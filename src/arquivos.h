510af7d7afcab2315f72835151cf4a65f08d7441
Alec Campos Aoki <alec.campsaoki@gmail.com>
2025-04-05 16:16:12 -0300

    Update arquivos.h

--- src/arquivos.h
+++ src/arquivos.h
@@ -1,12 +1,8 @@
-//
-// Created by alecc on 4/4/25.
-//
-
 #ifndef ARQUIVOS_H
     #define ARQUIVOS_H
 
+    typedef struct header_ HEADER;
+
+    bool arquivo_criar(char* nomeArquivo);
+
 #endif //ARQUIVOS_H
-
-typedef struct header_ HEADER;
-
-bool arquivo_criar(char* nomeArquivo);
