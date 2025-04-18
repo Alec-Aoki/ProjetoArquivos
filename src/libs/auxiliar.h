31f20da9f3efa7beaf9b44302bca5e3085929e75
Alec Campos Aoki <alec.campsaoki@gmail.com>
2025-04-18 18:24:49 -0300

    Update all

--- src/libs/auxiliar.h
+++ src/libs/auxiliar.h
@@ -1,8 +1,13 @@
 #ifndef AUXILIAR_H
     #define AUXILIAR_H
 
+    // Converte uma string numérica para um intero
     int str_to_int (char *str);
+
+    // Converte uma string numérica para um float
     float str_to_float (char *str);
+
+    // Aloca dinamicamente memória para uma string e adiciona delimitadores no inicio e final
     char *formata_string_registro (char *string, char *id);
 
 #endif
