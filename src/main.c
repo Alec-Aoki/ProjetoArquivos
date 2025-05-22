f0a53339e1dc72718e8b8ec0fd4693e53c2263ef
Alec Campos Aoki <alec.campsaoki@gmail.com>
2025-05-22 11:30:27 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,34 +10,31 @@ ALEC CAMPOS AOKI - 15436800
 JÕAO RICARDO DE ALMEIDA LUSTOSA - 15463697
 */
 
-int main(void){
+int main(void)
+{
     char op;
     char nomeArquivoBin[100];
     char nomeArquivoCSV[100];
 
     scanf("%c", &op);
-        
-    // Funcionalidade 1
-    if(op == '1'){
-        
+
+    switch (op)
+    {
+    case '1': // Funcionalidade 1
         scanf("%s", nomeArquivoCSV);
         scanf("%s", nomeArquivoBin);
-
-        arquivo_criar(nomeArquivoBin, nomeArquivoCSV);   
-    }
-        
-    // Funcionalidade 2
-    if (op == '2'){
+        arquivo_criar(nomeArquivoBin, nomeArquivoCSV);
+        break;
+    case '2': // Funcionalidade 2
         scanf("%s", nomeArquivoBin);
         arquivo_imprimir(nomeArquivoBin);
-    }
-
-    // Funcionalidade 3
-    if (op == '3'){
+        break;
+    case '3': // Funcionalidade 3
         scanf("%s", nomeArquivoBin);
         int quantBuscas;
         scanf("%d", &quantBuscas);
         arquivo_buscar(nomeArquivoBin, quantBuscas);
+        break;
     }
 
     return 0;
